c21bc124e47265c184c738e42ed2071ec6ab6676
Awinash <awinash.awsathy@peopletech.com>
2023-03-28 16:17:56 +0530

    ItienaryFromTickets.java file added

--- Hashing/ItienaryFromTickets.java
+++ Hashing/ItienaryFromTickets.java
@@ -3,14 +3,36 @@ package Hashing;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class ItienaryFromTickets {
+    public static String getStart(HashMap<String,String> tickets){
+        String start = "";
+        Map<String, String> reverseMap = new HashMap<>();
+        for(String key : tickets.keySet()){
+            reverseMap.put(tickets.get(key), key);
+        }
+        for(String ticket : tickets.keySet()){
+            if(!reverseMap.containsKey(ticket)){
+                start = ticket;
+            }
+        }
+        return start;
+    }
 
     public static void main(String[] args) {
-        Map<String, String> tickets = new HashMap<>();
-        tickets.put("chennai", "Bengaluru");
+        HashMap<String, String> tickets = new HashMap<>();
+        tickets.put("Chennai", "Bengaluru");
         tickets.put("Mumbai","Delhi");
         tickets.put("Goa","Chennai");
         tickets.put("Delhi","Goa");
+        String start = getStart(tickets);
+
+        while(tickets.containsKey(start)){
+            System.out.print(start+"->");
+            start = tickets.get(start);
+        }
+        System.out.print(start);
     }
 
     
