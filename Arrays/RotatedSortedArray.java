ca750689c452b3ad81f3a78dc9eef9406185c3ae
Awinash <awinash.awsathy@peopletech.com>
2023-04-05 17:02:16 +0530

    adding maxProfit.java

--- Arrays/RotatedSortedArray.java
+++ Arrays/RotatedSortedArray.java
@@ -1,5 +1,5 @@
 //There is an integer array nums sorted in ascending order (with distinct values).
-
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 /*
  * Input: nums = [4,5,6,7,0,1,2], target = 0
    Output: 4
