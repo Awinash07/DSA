f0c961627562f13fca3efca5243af938a7246b0a
Awinash <awinash.awsathy@peopletech.com>
2023-04-05 11:46:07 +0530

    adding containsDuplicates.java

---
+++ Arrays/ContainsDuplicates.java
@@ -0,0 +1,29 @@
+/*Given an integer array nums, return true if any value appears at least twice in the array,
+ and return false if every element is distinct. 
+ 
+ Input: nums = [1,2,3,1]
+
+ Output: true  */
+
+package Arrays;
+
+import java.util.Arrays;
+
+public class ContainsDuplicates {
+
+    public static boolean containsDuplicates(int nums[]){
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length-1 ; i++){
+            if(nums[i] == nums[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,1};
+        boolean value = containsDuplicates(nums);
+        System.out.println(value);
+    }
+    
+}
