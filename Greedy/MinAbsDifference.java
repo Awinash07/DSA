91bad6f336893225f47f78ac1aa814b34a4fdc7d
Awinash <awinash.awsathy@peopletech.com>
2023-04-13 14:57:40 +0530

    adding  Greedy/MinAbsDifference.java

---
+++ Greedy/MinAbsDifference.java
@@ -0,0 +1,35 @@
+/**
+ *  Min absolute difference pair :-
+ *  Given two arrays a & b of equal length n. pair each element of array a to an element in array b ,
+ *  such that sum s of absolute difference os all the pairs is minimum.
+ *
+ *  A = [1,2,3]
+ *  B = [2,1,3]
+ *  ans = 0
+ */
+
+
+package Greedy;
+
+import java.util.Arrays;
+
+public class MinAbsDifference {
+
+    public int minDiff(int a[] ,int b[]){
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int mindiff = 0;
+        for(int i = 0; i < a.length ;i++){
+            mindiff = mindiff + Math.abs(a[i] - b[i]);
+        }
+        return mindiff;
+    }
+
+    public static void main(String[] args){
+        int[] a = {4,1,8,7};
+        int[] b = {2,3,5,6};
+        MinAbsDifference minAbsDifferencen = new MinAbsDifference();
+        int result = minAbsDifferencen.minDiff(a,b);
+        System.out.println(result);
+    }
+}
