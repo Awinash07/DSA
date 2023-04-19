a18e93c1891dd0528f46618831de4dadccb24057
Awinash <awinash.awsathy@peopletech.com>
2023-04-19 12:44:09 +0530

    Arrays/RotatedSortedArray.java

--- Arrays/RotatedSortedArray.java
+++ Arrays/RotatedSortedArray.java
@@ -11,19 +11,45 @@ public class RotatedSortedArray {
 
     public static int rotatedSortedArray(int nums[], int target){
         int start = 0;
-        int end = nums.length; 
+        int end = nums.length;
         while(end > start){
             if(nums[start] == target) return start;
             start++;
         }
         return -1;
-    } 
+    }
+    public static int rotatedSortedArrayBinarySearch(int nums[], int target){
+        int high = nums.length - 1;
+        int low = 0 ;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] > nums[low]){
+                if(nums[low] < target && nums[mid] > target){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+
+            }else{
+                if(target > nums[mid] && target < nums[high]){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
 
     public static void main(String[] args) {
         int nums[] = {4,5,6,7,0,1,2};
-        int target = 0;
-        int result = rotatedSortedArray(nums, target);
-        System.out.println(result);
+        int target = 2;
+        //int result = rotatedSortedArray(nums, target);
+        int resultBT = rotatedSortedArrayBinarySearch(nums, target);
+        System.out.println(resultBT);
     }
     
 }
---
+++ Greedy/jobSequence.java
@@ -0,0 +1,14 @@
+/**
+ * Job sequencing problem
+ *
+ * Given an array of jobs where every job has a deadline and profit if the job is finished before the deadline.
+ * it is also given that every job takes a single job takes a single unit of time ,
+ * so the minimum possible deadline for any job is 1. Maximum the total profit if only one job can be schedule at time.
+ *
+ *
+ */
+package Greedy;
+
+public class jobSequence {
+}
+
