2139336d946c1aad82ce3cc6fd9f5de65764e75f
Awinash <awinash.awsathy@peopletech.com>
2023-04-21 15:23:14 +0530

    Strings/IsAnagram.java

--- Hashing/MajorityElement.java
+++ Hashing/MajorityElement.java
@@ -4,28 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MajorityElement {
-	public static void findMajorityElement(int arr[]){
+	public static void findMajorityElement(int[] arr){
 		Map<Integer, Integer> map = new HashMap<>();
-		for(int i = 0; i < arr.length; i++){
+		for (int j : arr) {
 			int count = 1;
-			if(!map.containsKey(arr[i])){
-				map.put(arr[i],count);
-			}else{
-				int num = map.get(arr[i]);
-				map.put(arr[i],num+1);
-			}	
+			if (!map.containsKey(j)) {
+				map.put(j, count);
+			} else {
+				int num = map.get(j);
+				map.put(j, num + 1);
+			}
 		}
 		for(Map.Entry<Integer, Integer> entry : map.entrySet()){
 			int maxLen = arr.length;
 			int fun = maxLen/3;
-			if(entry.getValue()>fun){
-				System.out.println(entry.getKey());
-			}
+			if(entry.getValue()>fun) System.out.println(entry.getKey());
 		}
 	}
 
-	public static void main(String args[]){
-		int arr[] = {1,2,3,4,5,5,5};
+	public static void main(String[] args){
+		int[] arr = {1,2,3,4,5,5,5};
 		findMajorityElement(arr);
 	}
 }
---
+++ Strings/IsAnagram.java
@@ -0,0 +1,41 @@
+/**
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ * Example 1:
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "rat", t = "car"
+ * Output: false
+ */
+
+package Strings;
+
+public class IsAnagram {
+    public static void main(String[] args) {
+        String a = "nagaram";
+        String b = "anagram";
+        boolean ans = isAnagram(a,b);
+        System.out.println(ans);
+    }
+
+    private static boolean isAnagram(String a, String b) {
+        int[] al = new int[256];
+        int[] bl = new int[256];
+        int index;
+        for(char ch : a.toCharArray()){
+            index = ch;
+            al[index]++;
+        }
+        for(char ch : b.toCharArray()){
+            index = ch;
+            bl[index]++;
+        }
+        for(int i=0 ; i < 256 ; i++){
+            if(al[i] != bl[i]) return false;
+        }
+        return true;
+    }
+}
