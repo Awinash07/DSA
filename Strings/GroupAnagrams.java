70904d7790b53b96e9e0a14f4dc8ede1cefbef88
Awinash <awinash.awsathy@peopletech.com>
2023-05-17 13:04:02 +0530

    modifying group anagrams

--- Strings/GroupAnagrams.java
+++ Strings/GroupAnagrams.java
@@ -23,9 +23,7 @@ public class GroupAnagrams {
             char charArray[] = s.toCharArray();
             Arrays.sort(charArray);
             String sorted = new String(charArray);
-            if(!map.containsKey(sorted)){
-                map.put(sorted, new LinkedList<String>());
-            }
+            map.computeIfAbsent(sorted, e -> new LinkedList<>());
             map.get(sorted).add(s);
         }
         return new LinkedList<>(map.values());
